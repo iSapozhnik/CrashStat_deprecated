6fc4adad26724171d2aa20300f662de445430039
Ivan Sapozhnik <sapozhnik.ivan@gmail.com>
2018-12-09 22:39:59 +0100

    working on quering and saving data

--- Sources/App/Controllers/AppInfoController.swift
+++ Sources/App/Controllers/AppInfoController.swift
@@ -14,16 +14,35 @@ final class AppInfoController {
     }
     
     func crash(_ req: Request) throws -> Future<Crash> {
-        let android = AppInfo.query(on: req).filter(\.appPlatform == "android").count()
-        return AppInfo.query(on: req).filter(\.appPlatform == "ios").count().and(android).map { (ios, android) -> Crash in
+        let androidCrashes = AppInfo.query(on: req).filter(\.appPlatform == "android").all()
+        let iosCrashes = AppInfo.query(on: req).filter(\.appPlatform == "ios").all()
+        
+        return [androidCrashes, iosCrashes].flatten(on: req).map({ array -> (Crash) in
+            var ios = 0
+            var android = 0
+            
+            array.forEach({ appInfosArray in
+                appInfosArray.forEach({ info in
+                    if info.appPlatform == "ios" {
+                        ios += info.crashes
+                    } else {
+                        android += info.crashes
+                    }
+                })
+            })
+            
             return Crash(iosCount: ios, androidCount: android)
-        }
+        })
+        
+        
+//        let android = AppInfo.query(on: req).filter(\.appPlatform == "android").count()
+//        return AppInfo.query(on: req).filter(\.appPlatform == "ios").count().and(android).map { (ios, android) -> Crash in
+//            return Crash(iosCount: ios, androidCount: android)
+//        }
     }
     
     func createIOSCrashProd(_ req: Request) throws -> Future<AppInfo> {
         return try req.content.decode(AppInfo.self).flatMap { appInfo in
-//            appInfo.platform = Platform.ios.rawValue
-//            appInfo.app = App.prod.rawValue
             return appInfo.save(on: req)
         }
     }
@@ -51,22 +70,46 @@ final class AppInfoController {
 //            return info.update(on: req)
 //        }
 
-        return try req.content.decode(AppInfo.self).flatMap { appInfo in
-//            appInfo.platform = Platform.ios.rawValue
-//            appInfo.app = App.dev.rawValue
-
-
-            return appInfo.save(on: req)
+        
+//        let iOSCrashes = Future<Int>
+//        let androidCrashes = Future<Int>
+//        return [iOSCrashes, androidCrashes].flatten().map { return $0 }
+        
+        let decode = try req.content.decode(AppInfo.self)
+        return decode.flatMap { appInfo -> EventLoopFuture<AppInfo> in
+            let query = AppInfo.query(on: req).filter(\AppInfo.issueId == appInfo.issueId).first()
+            return query.flatMap({ existingIssue -> EventLoopFuture<AppInfo> in
+                if let issueId = existingIssue?.id {
+                    appInfo.id = issueId
+                    return appInfo.update(on: req)
+                } else {
+                    return appInfo.save(on: req)
+                }
+            })
         }
+        
+//        return try req.content.decode(AppInfo.self).flatMap { appInfo in
+//            return AppInfo.query(on: req).filter(\AppInfo.issueId == appInfo.issueId).first().flatMap({ info -> AppInfo in
+//                guard let info = info, let infoId = info.id else { return appInfo.save(on: req) }
+//                info.update(on: req)
+//            })
+//        }
     }
     
     ////////////////////
     
     func createAndroidCrashProd(_ req: Request) throws -> Future<AppInfo> {
-        return try req.content.decode(AppInfo.self).flatMap { appInfo in
-//            appInfo.platform = Platform.android.rawValue
-//            appInfo.app = App.prod.rawValue
-            return appInfo.save(on: req)
+        let decode = try req.content.decode(AppInfo.self)
+        return decode.flatMap { appInfo -> EventLoopFuture<AppInfo> in
+            let query = AppInfo.query(on: req).filter(\AppInfo.issueId == appInfo.issueId).first()
+            return query.flatMap({ existingIssue -> EventLoopFuture<AppInfo> in
+                if let issueId = existingIssue?.id {
+                    appInfo.id = issueId
+                    return appInfo.update(on: req)
+                } else {
+                    return appInfo.save(on: req)
+                }
+            })
         }
     }
     
