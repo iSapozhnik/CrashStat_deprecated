18668aa43c0181e6c3623eb1e30edd06486f470f
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2018-12-09 21:27:22 +0100

    Updating models

--- Sources/App/Controllers/AppInfoController.swift
+++ Sources/App/Controllers/AppInfoController.swift
@@ -14,41 +14,48 @@ final class AppInfoController {
     }
     
     func crash(_ req: Request) throws -> Future<Crash> {
-        let android = AppInfo.query(on: req).filter(\.platform == "android").count()
-        return AppInfo.query(on: req).filter(\.platform == "ios").count().and(android).map { (ios, android) -> Crash in
+        let android = AppInfo.query(on: req).filter(\.appPlatform == "android").count()
+        return AppInfo.query(on: req).filter(\.appPlatform == "ios").count().and(android).map { (ios, android) -> Crash in
             return Crash(iosCount: ios, androidCount: android)
         }
     }
     
     func createIOSCrashProd(_ req: Request) throws -> Future<AppInfo> {
         return try req.content.decode(AppInfo.self).flatMap { appInfo in
-            appInfo.platform = Platform.ios.rawValue
-            appInfo.app = App.prod.rawValue
+//            appInfo.platform = Platform.ios.rawValue
+//            appInfo.app = App.prod.rawValue
             return appInfo.save(on: req)
         }
     }
     
     func createIOSCrashAlpha(_ req: Request) throws -> Future<AppInfo> {
         return try req.content.decode(AppInfo.self).flatMap { appInfo in
-            appInfo.platform = Platform.ios.rawValue
-            appInfo.app = App.alpha.rawValue
+//            appInfo.platform = Platform.ios.rawValue
+//            appInfo.app = App.alpha.rawValue
             return appInfo.save(on: req)
         }
     }
     
     func createIOSCrashBeta(_ req: Request) throws -> Future<AppInfo> {
         return try req.content.decode(AppInfo.self).flatMap { appInfo in
-            appInfo.platform = Platform.ios.rawValue
-            appInfo.app = App.beta.rawValue
+//            appInfo.platform = Platform.ios.rawValue
+//            appInfo.app = App.beta.rawValue
             return appInfo.save(on: req)
         }
     }
     
     func createIOSCrashDev(_ req: Request) throws -> Future<AppInfo> {
         print("CRASH!")
+
+//        AppInfo.query(on: req).filter(\.issueId == "issueId").first().flatMap { info -> AppInfo? in
+//            return info.update(on: req)
+//        }
+
         return try req.content.decode(AppInfo.self).flatMap { appInfo in
-            appInfo.platform = Platform.ios.rawValue
-            appInfo.app = App.dev.rawValue
+//            appInfo.platform = Platform.ios.rawValue
+//            appInfo.app = App.dev.rawValue
+
+
             return appInfo.save(on: req)
         }
     }
@@ -57,32 +64,32 @@ final class AppInfoController {
     
     func createAndroidCrashProd(_ req: Request) throws -> Future<AppInfo> {
         return try req.content.decode(AppInfo.self).flatMap { appInfo in
-            appInfo.platform = Platform.android.rawValue
-            appInfo.app = App.prod.rawValue
+//            appInfo.platform = Platform.android.rawValue
+//            appInfo.app = App.prod.rawValue
             return appInfo.save(on: req)
         }
     }
     
     func createAndroidCrashAlpha(_ req: Request) throws -> Future<AppInfo> {
         return try req.content.decode(AppInfo.self).flatMap { appInfo in
-            appInfo.platform = Platform.android.rawValue
-            appInfo.app = App.alpha.rawValue
+//            appInfo.platform = Platform.android.rawValue
+//            appInfo.app = App.alpha.rawValue
             return appInfo.save(on: req)
         }
     }
     
     func createAndroidCrashBeta(_ req: Request) throws -> Future<AppInfo> {
         return try req.content.decode(AppInfo.self).flatMap { appInfo in
-            appInfo.platform = Platform.android.rawValue
-            appInfo.app = App.beta.rawValue
+//            appInfo.platform = Platform.android.rawValue
+//            appInfo.app = App.beta.rawValue
             return appInfo.save(on: req)
         }
     }
     
     func createAndroidCrashDev(_ req: Request) throws -> Future<AppInfo> {
         return try req.content.decode(AppInfo.self).flatMap { appInfo in
-            appInfo.platform = Platform.android.rawValue
-            appInfo.app = App.dev.rawValue
+//            appInfo.platform = Platform.android.rawValue
+//            appInfo.app = App.dev.rawValue
             return appInfo.save(on: req)
         }
     }
--- Sources/App/Models/AppInfo.swift
+++ Sources/App/Models/AppInfo.swift
@@ -41,47 +41,48 @@ enum App: String {
     case dev
 }
 
-final class Payload: SQLiteModel {
-    var id: Int?
-    
-    var display_id: Int
-    var title: String
-    var method: String
-    var impact_level: Int
-    var crashes_count: Int
-    var impacted_devices_count: Int
-    var url: String
-    
-    init(id: Int? = nil, display_id: Int, title: String, method: String, impact_level: Int, crashes_count: Int, impacted_devices_count: Int, url: String) {
-        self.id = id
-        self.display_id = display_id
-        self.title = title
-        self.method = method
-        self.impact_level = impact_level
-        self.crashes_count = crashes_count
-        self.impacted_devices_count = impacted_devices_count
-        self.url = url
-    }
-}
-
 final class AppInfo: SQLiteModel {
     var id: Int?
     
-    var event: String
-    var payload_type: String
-    var payload: Payload
-    var platform: String?
-    var app: String?
+    var issueId: String
+    var issueTitle: String
+    var appName: String
+    var appPlatform: String
+    var appId: String
+    var latestAppVersion: String
+    var crashPercentage: Int
+    var crashes: Int
     
-    init(id: Int? = nil, event: String, payload_type: String, payload: Payload, platform: String?, app: String?) {
-        self.id = id
-        self.event = event
-        self.payload_type = payload_type
-        self.payload = payload
-        self.platform = platform
-        self.app = app
+    init(issueId: String, issueTitle: String, appName: String, appPlatform: String, appId: String, latestAppVersion: String, crashPercentage: Int, crashes: Int) {
+        self.issueId = issueId
+        self.issueTitle = issueTitle
+        self.appName = appName
+        self.appId = appId
+        self.appPlatform = appPlatform
+        self.latestAppVersion = latestAppVersion
+        self.crashPercentage = crashPercentage
+        self.crashes = crashes
     }
 }
+
+//final class AppInfo: SQLiteModel {
+//    var id: Int?
+//    
+//    var event: String
+//    var payload_type: String
+//    var payload: Payload
+//    var platform: String?
+//    var app: String?
+//    
+//    init(id: Int? = nil, event: String, payload_type: String, payload: Payload, platform: String?, app: String?) {
+//        self.id = id
+//        self.event = event
+//        self.payload_type = payload_type
+//        self.payload = payload
+//        self.platform = platform
+//        self.app = app
+//    }
+//}
 
 final class Crash: Content {
     var ios: Int
